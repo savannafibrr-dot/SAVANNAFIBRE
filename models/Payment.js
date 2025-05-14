98ec20e5dcb6db0b009a3e934b521abf7cf7ec55
unknown <derrickmuteti2001@gmail.com>
2025-05-14 13:08:06 +0300

    made more changes

--- models/Payment.js
+++ models/Payment.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const PaymentMethodSchema = new mongoose.Schema({
   methodName: { type: String, required: true },
   iconClass: { type: String, required: true }, // e.g., 'fas fa-mobile-alt'
-  steps: [{ type: String, required: true }],
+  steps: [{ 
+    type: String, 
+    required: true,
+    // Steps can contain HTML for formatting (bold, italic, color, etc.)
+    // Example: "Press <strong>*150*00#</strong> then <span style='color: red'>ok</span>"
+  }],
   order: { type: Number, default: 0 },
 }, { timestamps: true });
 
